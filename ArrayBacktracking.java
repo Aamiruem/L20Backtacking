bb12513ba2029ba3a3c6c35130c10f94f0c368bc
Aamir Hussain <aamirhussainazad786@gmail.com>
2024-02-08 14:01:52 +0530

    commit changes

--- ArrayBacktracking.java
+++ ArrayBacktracking.java
@@ -21,7 +21,7 @@ public class ArrayBacktracking {
 
   public static void main(String[] args) {
     int arr[] = new int[5];
-    changeArr(arr, 0, 1);
+    changeArr(arr, 0, 2);
     printArr(arr);
   }
 }

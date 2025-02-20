5f7a08997aa6a1cbc789e1651e9f34ddbf1b7917
“Aamir <aamirhussainazad786@gmail.com>
2025-02-20 11:22:58 +0530

    commit add

---
+++ Subsets.java
@@ -0,0 +1,23 @@
+package L20BackTarcking;
+
+public class Subsets {
+
+  public static void findSubsets(String str, String ans, int i) {
+    // base case
+    if (i == str.length()) {
+      System.out.print(" [" + ans.trim() + "] ");
+      return;
+    }
+    // recursion
+    // yes choice
+    findSubsets(str, ans + " " + str.charAt(i), i + 1);
+
+    // no choice
+    findSubsets(str, ans, i + 1);
+  }
+
+  public static void main(String[] args) {
+    String str = "abc";
+    findSubsets(str, " ", 0);
+  }
+}
